97f9c26bef30e35d47f3793a5536e2abd2d8df88
Kelvin Ferri Brancalhao <kelvinbrancalhao@gmail.com>
2021-02-19 16:24:25 -0300

    Commit de criaçào do projeto

---
+++ poenabio/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+
+        <title>Poe na Bio</title>
+      
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  )
+}
